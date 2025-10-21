3b612e5b80673fb865550cde9ce4a77f2a7bbea9
Elang Alfarez <keephard055@gmail.com>
2025-10-21 17:52:39 +0700

    added everything, scafolded everything

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
-// next.config.mjs — ES module variant
 export default {
-  basePath: '/treasurehunt',
-  assetPrefix: '/treasurehunt/',
+  basePath: '/spookyhunt',
+  assetPrefix: '/spookyhunt/',
   reactStrictMode: true,
-  // Optional: if you want a smaller, self-contained production build (see notes below)
-  // output: 'standalone',
 };
